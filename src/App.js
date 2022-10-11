07b80b9d305cbb4a347c8ae5444a6b21e2e4700b
Favour-Cay <iamfavourkeshi@gmail.com>
2022-10-11 22:12:46 +0100

    my calculator app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  //  const [num , setNum] = useState("");
+  //  const calcBtns = [];
+  //  []
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+  
+    <div className="calculator">
+         <label class="switch">
+    <input type="checkbox"/>
+    <span class="slider round"></span>
+  </label>
+<h1>CALCULATOR</h1>
+      <input type="text" className="field" cellspacing="1" />
+      <table>
+        <tbody>
+          <tr>
+            <td>
+              <button className="calc-button" id="zero-button">A</button>
+            </td>
+            <td>
+              <button className="calc-button">%</button>
+            </td>
+            <td>
+              <button className="calc-button">C</button>
+            </td>
+            <td>
+              <button className="calc-button">=</button>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <button className="calc-button">7</button>
+            </td>
+            <td>
+              <button className="calc-button">8</button>
+            </td>
+            <td>
+              <button className="calc-button">9</button>
+            </td>
+            <td>
+              <button className="calc-button">/</button>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <button className="calc-button">4</button>
+            </td>
+            <td>
+              <button className="calc-button">5</button>
+            </td>
+            <td>
+              <button className="calc-button">6</button>
+            </td>
+            <td>
+              <button className="calc-button" value="-" onclick="">
+              </button>
+            </td>
+          </tr>
+
+          <tr>
+            <td>
+              <button className="calc-button">1</button>
+            </td>
+            <td>
+              <button className="calc-button">2</button>
+            </td>
+            <td>
+              <button className="calc-button">3</button>
+            </td>
+            <td>
+              <button className="calc-button">+</button>
+            </td>
+          </tr>
+
+          <tr>
+            <td>
+              <button className="calc-button">.</button>
+            </td>
+            <td>
+              <button className="calc-button">*</button>
+            </td>
+            <td colSpan="2" style={{backgroundColor:'red',width:'100%'}}>
+              <button className="calc-button" id="zero-button">0</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
